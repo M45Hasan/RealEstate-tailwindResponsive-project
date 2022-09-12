c9a7befc972d671549baa903f53c7109879bd9c2
M45Hasan <mmhasan045@gmail.com>
2022-09-12 13:16:53 +0600

    Project__done
    real estate tailwind responsive

---
+++ js/tailwind.config.js
@@ -0,0 +1,42 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        white:"rgba(238, 237, 222, 1)",
+        red: "#F40404",
+        yellow: "rgb(250 204 21)",
+        bag: "rgba(238, 237, 222, 1)",
+        bag1: "#FFFFFF",
+        bag4:"rgba(255, 255, 255, 1)",
+        tx1: "#203239",
+        bder: "rgba(255, 255, 255, 1)",
+        bdr: "rgba(0, 0, 0, .5)",
+        but1: "rgba(32, 50, 57, 1)",
+        bbo: "rgba(214, 213, 199, 1)",
+        angb: "rgba(214, 213, 199, 0.3)",
+        but2: "rgba(255, 117, 39, 0.2)",
+        org: "rgba(255, 117, 39, 1)",
+        
+
+
+        pls: "rgba(255, 117, 39, 1)",
+        bo: "rgba(238, 237, 222, 1)",
+        sr: "rgba(214, 213, 199, 1)",
+      },
+      maxWidth: {
+        conatiner: "1164px",
+      },
+      fontFamily: {
+        pop: "Poppins",
+      },
+      backgroundImage: {
+        ban: "url('hom1.png')",
+        fv2: "url('fv2.png')",
+        res1: "url('res1.png')",
+        res2: "url('res2.png')",
+        res3: "url('res3.png')",
+      },
+    },
+  },
+};
